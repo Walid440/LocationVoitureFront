3cb6d919b5b751b875317497fb8f247b3d626220
walid <walid.gammoudi@esprit.tn>
2023-07-07 20:00:53 +0200

    ajouter reservation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { OwlModule } from 'ngx-owl-carousel';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { BrowserModule } from '@angular/platform-browser';
 import { PrincipalComponent } from './principal/principal.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
