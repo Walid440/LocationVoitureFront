ec7d81246a58b130caf2d0a44957d650438b1159
walid <walid.gammoudi@esprit.tn>
2023-08-30 12:08:38 +0200

    Modif chat

--- src/assets/chat.js
+++ src/assets/chat.js
@@ -1,6 +1,111 @@
-const { execute } = require("@angular-devkit/build-angular/src/builders/extract-i18n");
+ 
+  function heartButton(){
+  
+    let UserHtml='<p class="fas fa-heart" style="color:red;"></p>';
+  $("#chatbox").append(UserHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
+ 
+ 
+
+  }
+  
+ 
+function sendButton(){
+  let ch="";
+  
+let val= $("#textInput").val(); 
+if(val=="hello")
+{
+ ch= "Hiiiiiiii!";
+}
+else if(val=="cv"){
+  ch="vous etes les bienvenue!!!"
+
+}
+let UserHtml='<p class="botText"><span>'+val+'</span></p>';
+ 
+
+ 
+$("#chatbox").append(UserHtml);
+$("#chatbox").append(ch); 
+$("#textInput").val(""); 
+ //document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+ 
+
+    
+ 
+  
+// document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+ //document.getElementById("userInput").scrollIntoView(true);
+}
 
 
+
+var coll=document.getElementsByClassName("collapsible");
+
+for (let i=0;i<coll.length;i++)
+{
+  coll[i].addEventListener("click",function(){
+    this.classList.toggle("active");
+    const content = document.querySelector('.content');
+    if (content.style.maxHeight) {
+    content.style.maxHeight = null;   
+     }
+    else  {  
+       
+content.style.maxHeight = content.scrollHeight + 'px';
+ 
+
+
+
+var coll1=document.getElementsByClassName("textInput");
+
+for (let i=0;i<coll1.length;i++)
+{
+  coll1[i].addEventListener("keypress",function(e){
+    this.classList.toggle("active");
+    const content = document.querySelector('.content');
+  
+   
+    if(e.key=="Enter")
+    { 
+   sendButton();
+    }
+  })}
+
+
+
+
+
+
+ const day=new Date();
+ const hours=day.getHours();
+ const minutes=day.getMinutes();
+ const secondes=day.getSeconds();
+ if(hours<10)
+ {
+  hours="0"+hours;
+ }if(minutes<10)
+ {
+  minutes="0"+minutes;
+ }
+  
+let times=hours+":"+minutes;
+
+
+$("#chat-timestamp").append(times);
+document.getElementById("userInput").scrollIntoView(false);
+     
+    
+ 
+ 
+
+}
+  
+  })
+}
 
 function c() {
   var coll = document.getElementById("s1");
@@ -125,113 +230,6 @@ function load() {
 
 
 
-var coll=document.getElementsByClassName("collapsible");
-
-for (let i=0;i<coll.length;i++)
-{
-  coll[i].addEventListener("click",function(){
-    this.classList.toggle("active");
-    const content = document.querySelector('.content');
-    if (content.style.maxHeight) {
-    content.style.maxHeight = null;   
-     }
-    else  {  
-       
-content.style.maxHeight = content.scrollHeight + 'px';
-  
-
-
-
-
-var coll1=document.getElementsByClassName("textInput");
-
-for (let i=0;i<coll1.length;i++)
-{
-  coll1[i].addEventListener("keypress",function(e){
-    this.classList.toggle("active");
-    const content = document.querySelector('.content');
-  
-   
-    if(e.key=="Enter")
-    { 
-   sendButton();
-    }
-  })}
-
-
-
-
-
-
- const day=new Date();
- const hours=day.getHours();
- const minutes=day.getMinutes();
- const secondes=day.getSeconds();
- if(hours<10)
- {
-  hours="0"+hours;
- }if(minutes<10)
- {
-  minutes="0"+minutes;
- }
-  
-let times=hours+":"+minutes;
-
-
-$("#chat-timestamp").append(times);
-document.getElementById("userInput").scrollIntoView(false);
-     
-    
- 
  
 
-}
-  
-  })
-  
- 
-  function heartButton(){
-  
-    let UserHtml='<p class="fas fa-heart" style="color:red;"></p>';
-  $("#chatbox").append(UserHtml);
-  document.getElementById("chat-bar-bottom").scrollIntoView(true);
- 
- 
-
-  }
-  
- 
-function sendButton(){
-  let ch="";
-  
-let val= $("#textInput").val(); 
-if(val=="hello")
-{
- ch= "Hiiiiiiii!";
-}
-else if(val=="cv"){
-  ch="vous etes les bienvenue!!!"
-
-}
-let UserHtml='<p class="botText"><span>'+val+'</span></p>';
- 
-
- 
-$("#chatbox").append(UserHtml);
-$("#chatbox").append(ch); 
-$("#textInput").val(""); 
- //document.getElementById("chat-bar-bottom").scrollIntoView(true);
-
- 
-
-    
- 
-  
-// document.getElementById("chat-bar-bottom").scrollIntoView(true);
-
- //document.getElementById("userInput").scrollIntoView(true);
-}
-
-
-}
 }
